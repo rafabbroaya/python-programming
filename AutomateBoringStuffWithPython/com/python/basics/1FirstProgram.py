081eb0725df739fcf8b1d5ada70de2b1045235c8
rafael <rafael@rafael-Latitude-E6520>
2016-05-16 00:18:18 -0500

    Init

---
+++ AutomateBoringStuffWithPython/com/python/basics/1FirstProgram.py
@@ -0,0 +1,14 @@
+'''
+Created on May 15, 2016
+
+@author: rafael
+'''
+print('Hello world!')
+print('What is your name?') #ask for thei Name
+myName = input()
+print('It is goof to meet you, '+ myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')
+myAge=input()
+print('You will be ' +str(int(myAge) + 1) + ' in a year')
