a7491946c2898ac683354873f636d4340cfb723f
rafael <rafael@rafael-Latitude-E6520>
2016-05-20 00:23:24 -0500

    flow control (at home)

--- AutomateBoringStuffWithPython/com/python/flowControl/Statements.py
+++ AutomateBoringStuffWithPython/com/python/flowControl/Statements.py
@@ -13,4 +13,43 @@ if name == 'erick':
     print('Hello erick')
 elif name == 'jose':
     print('Hello jose')
+
+'''If elif else'''
+age = input('Enter his age!\n')
+age = int(age)
+if age < 10:
+    print('You are a kiddo')
+elif age < 20:
+    print('You are young')
+else:
+    print('You are adult')
+    
+'''while loop'''
+spam=0
+while spam<5:
+    print(spam)
+    spam = spam +1
+
+'''Break statement'''
+while True:
+    name = input('please type your name.')
+    if name == 'rafa':
+        break
+print('thank you')
+
+'''continue statement'''
+while True:
+    name = input('Enter your name\n')
+    if name != 'rafa':
+        continue
+    password = input('Hello rafa. What is the password? (It is a name)\n')
+    if password == 'password':
+        break
+print('Access granted')
+
+'''truthy and falsey'''
+
+namet = ''
+while not namet:
+    namet = input('enter your no name')
     
---
+++ AutomateBoringStuffWithPython/com/python/flowControl/forLoop.py
@@ -0,0 +1,23 @@
+for i in range(5):
+    print(i)
+
+print('-----------------------------')
+
+for i in range(0,10,2):
+    print(i)
+
+print('-----------------------------')
+
+for i in range (10,-1,-2):
+    print(i)    
+    
+import random, sys
+from math import cos
+
+print('-----------------------------')
+
+for i in range(5):
+    print(random.randint(0,10))
+
+sys.exit()
+print(str(cos(14)))
